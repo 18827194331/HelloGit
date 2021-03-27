11f1b6a89290e5ce40cd18f17e0684e22edfa512
th <1970958494@qq.com>
2021-03-27 15:21:17 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/th/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.th.git;
+
+/**
+ * @author shkstart
+ * @create 2021-03-27 15:11
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
